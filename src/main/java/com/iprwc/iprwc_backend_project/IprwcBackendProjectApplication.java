2d3f31c6b2d92debf8d9b051ecf250f2257f10a3
ThomasPijper <s1141675@student.hsleiden.nl>
2023-12-10 13:37:48 +0100

    Initial commit

---
+++ src/main/java/com/iprwc/iprwc_backend_project/IprwcBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.iprwc.iprwc_backend_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprwcBackendProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IprwcBackendProjectApplication.class, args);
+    }
+
+}
